e2fad82cc302b1f580e27d072d5bb1caeafe4ff7
Savyasachi <jsavyasachi@gmail.com>
2019-02-04 11:57:02 -0800

    Code fix
    Code sometimes used to include no transactions in a block. Fixed

--- SampleChain.py
+++ SampleChain.py
@@ -1,4 +1,3 @@
-#Blockchain Question
 #input integer array startBalances
 #input array[] pendingTransactions= [fromAddress,toAddress,value]
 #input int blockSize
@@ -6,28 +5,27 @@
 
 import hashlib
 
-
 def getLatestBlock(startBalances, pendingTransactions, blockSize):
     currentHash = '0000000000000000000000000000000000000000'
 
-    while (len(pendingTransactions) != 0):
-        previousHash = currentHash
+    while (pendingTransactions):
         currentTransactions = validateTransactions(startBalances, pendingTransactions, blockSize)
+        if(not currentTransactions):
+            break
+        previousHash = currentHash
         nonce = findNonce(previousHash, currentTransactions)
         currentHash = findBlockHash(previousHash, currentTransactions,nonce)
+        latestBlock=currentHash + ', ' + previousHash + ', ' + str(nonce) + ', ' + str(currentTransactions)
 
-    return currentHash + ', ' + previousHash + ', ' + str(nonce) + ', ' + str(currentTransactions)
-
+    return latestBlock
 
 def sha1(text):
     s = hashlib.sha1()
     s.update(text.encode('utf-8'))
     return s.hexdigest()
 
-
 def findBlockHash(prevBlockHash, transactions,nonce):
     return sha1(prevBlockHash + ', ' + str(nonce) + ', ' + str(transactions))
-
 
 def findNonce(prevBlockHash, transactions):
     found = 0
@@ -41,18 +39,16 @@ def findNonce(prevBlockHash, transactions):
 
     return nonce
 
-
 def validateTransactions(startBalances, pendingTransactions, blockSize):
     approvedTransactions = []
     count = 0
-    while (count < blockSize) or pendingTransactions:
+    while (count < blockSize) and pendingTransactions:
         if startBalances[pendingTransactions[0][0]] >= pendingTransactions[0][2]:
-            s = approvedTransactions.append(pendingTransactions[0])
+            approvedTransactions.append(pendingTransactions[0])
             updateBalances(startBalances, pendingTransactions[0])
         del pendingTransactions[0]
         count += 1
     return approvedTransactions
-
 
 def updateBalances(startBalances, currentTransactions):
     startBalances[currentTransactions[1]] = startBalances[currentTransactions[1]] + currentTransactions[2]
@@ -60,6 +56,8 @@ def updateBalances(startBalances, currentTransactions):
 
 def main():
     print(getLatestBlock([5, 0, 0], [[0, 1, 5], [1, 2, 5]], 2))
+    print(getLatestBlock([1,7], [[1, 0, 4], [1, 0, 3], [1, 0, 2]], 2))
+    print(getLatestBlock([3, 10, 10, 3], [[3,2,2], [2,3,5], [3,2,4], [3,0,2], [1,2,2]], 2))
 
 if __name__ == '__main__':
     main()
